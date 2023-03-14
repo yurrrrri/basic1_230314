ef0899e07bf01512af9cb950ef0b80aee38fd9eb
yurrrrri <yu_ri_son@naver.com>
2023-03-14 11:11:30 +0900

    HomeController

---
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -0,0 +1,14 @@
+package com.ll.basic1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home/main")
+    @ResponseBody
+    public String showMain(){
+        return "안녕하세요.";
+    }
+}
