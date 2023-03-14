ee990ae203a0a55153e249f6be96cce62ef4ee60
yurrrrri <yu_ri_son@naver.com>
2023-03-14 11:44:39 +0900

    increase

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -6,9 +6,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+
+    private int count;
+
+    public HomeController() {
+        count = -1;
+    }
+
     @GetMapping("/home/main")
     @ResponseBody
     public String showMain(){
         return "안녕하세요.";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showMain2(){
+        return "반갑습니다.";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showMain3(){
+        return "즐거웠습니다.";
+    }
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public String showIncrease(){
+        count++;
+        return Integer.toString(count);
     }
 }
