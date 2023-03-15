6ef9ac6124ebca5d3e598301f96aa0efcb0ededf
yurrrrri <yu_ri_son@naver.com>
2023-03-15 10:37:26 +0900

    login

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class HomeController {
@@ -104,20 +102,39 @@ public class HomeController {
     public int showCookieIncrease(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         int countInCookie = 0;
 
-        if (req.getCookies() != null) {
+        if(req.getCookies() != null){
             countInCookie = Arrays.stream(req.getCookies())
                     .filter(cookie -> cookie.getName().equals("count"))
-                    .map(cookie -> cookie.getValue())
+                    .map(Cookie::getValue)
                     .mapToInt(Integer::parseInt)
                     .findFirst()
-                    .orElse(0);
+                    .orElse(0);    // or getAsInt();
         }
+        int newCountInCookie = countInCookie+1;
 
-        int newCountInCookie = countInCookie + 1;
-
-        resp.addCookie(new Cookie("count", newCountInCookie + ""));
+        resp.addCookie(new Cookie("count", newCountInCookie+""));
 
         return newCountInCookie;
+    }
+
+    @GetMapping("/member/login")
+    @ResponseBody
+    public Map login(String username, String password){
+        Map<String, String> map = new LinkedHashMap<>();
+
+        if(username.equals("user1") && password.equals("1234")){
+            map.put("resultCode", "S-1");
+            map.put("msg", "user1 님 환영합니다.");
+            return map;
+        } else if(username.equals("user1") && !password.equals("1234")){
+            map.put("resultCode", "F-1");
+            map.put("msg", "비밀번호가 일치하지 않습니다.");
+            return map;
+        } else {
+            map.put("resultCode", "F-2");
+            map.put("msg", "%s(은)는 존재하지 않는 회원입니다.".formatted(username));
+            return map;
+        }
     }
 }
 
